532b2f4adefae9acf4e94a56762cba1f9ce89184
William Showalter <williamshowalter@gmail.com>
2015-02-18 14:53:30 -0900

    Initial Commit

---
+++ Bowling Kata/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bowling Kata
+//
+//  Created by William Showalter on 15/02/18.
+//  Copyright (c) 2015 William Showalter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
